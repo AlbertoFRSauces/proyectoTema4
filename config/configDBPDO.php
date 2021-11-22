2423d99fd8e671961356c050fab22e7c6a760dc2
Alberto <alberto.ferram@educa.jcyl.es>
2021-11-22 22:03:04 +0100

    22/11/2021 Fix README Fix configPDO Modified index, botones nuevos para ejecutar scripts de creacion, carga y borrado. Add archivos para mostrar scripts de creacion, carga y borrado.

--- config/configDBPDO.php
+++ config/configDBPDO.php
@@ -1,10 +1,10 @@
 <?php
     //CONFIGURACION DE DESARROLLO DE CASA
-    
+    /*
     define('HOST', 'mysql:host=192.168.1.107;dbname=DB207DWESProyectoTema4');//Direccion IP del host y nombre de la base de datos
     define('USER', 'User207DWESProyectoTema4');//Nombre del usuario de la base de datos
     define('PASSWORD', 'P@ssw0rd');//Contraseña del usuario de la base de datos
-    
+    */
 
     //CONFIGURACION DE DESARROLLO DE CLASE
     /*
@@ -14,11 +14,11 @@
     */
     
     //CONFIGURACION DE EXPLOTACION 1&1
-    /*
+    
     define('HOST', 'mysql:host=db5005797478.hosting-data.io;dbname=dbs4868800');//Direccion IP del host y nombre de la base de datos
     define('USER', 'dbu1194752');//Nombre del usuario de la base de datos
     define('PASSWORD', 'daw2_Sauces');//Contraseña del usuario de la base de datos
-    */
+    
     
     //CONFIGURACION DE DESARROLLO CLASE LOCAL
     /*
